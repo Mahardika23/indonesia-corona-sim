04d6886603dbf85f7cee92fff0f8171ac827a398
Arief Utama <arief.utama@gmail.com>
2020-04-12 13:14:01 +0700

    Change Heal/Die with a single function - Adding comments and fixed data-struct minor

--- CoronaSimulator.py
+++ CoronaSimulator.py
@@ -16,36 +16,58 @@ from random import seed,randint
 from collections import namedtuple
 from recordtype import recordtype
 
-SimConfig = namedtuple('SimConfig', 'ro hosprob isoprob medcnt dstart dend hstart hend')
+# Configuration by Cluster
+# ro = r0 factor
+# hosprob = hospitalization probability
+# isoprob = isolation probability
+# medcnt = medical facilities count (bed-count)
+# dstart = start range of dead (in days)
+# dend = end range of dead (in days)
+# hstart = start range of heal (in days)
+# dend = end range of heal (in days)
+Config = namedtuple('Config', 'ro hosprob isoprob medcnt dstart dend hstart hend')
 Config = {
-        'Jakarta' : SimConfig(1, 50, 30, 1000, 5, 15, 14, 30), 
-        'Bandung' : SimConfig(1, 50, 50, 1000, 5, 15, 14, 30)
+        'Jakarta' : Config(1, 50, 30, 1000, 5, 15, 14, 30), 
+        'Bandung' : Config(1, 50, 50, 1000, 5, 15, 14, 30)
         }
+
+# Recording data structure
+# total = total patient count
+# infect = total still has infecting probablity
+# hosp = hospitalized patient count
+# isol = isolated patient count
+# danger = patient not hospitalized not isolated
+# died = total death 
+# heal = total healed
+# XXX: Total available Medical fascility for Hospitalization = Config.medcnt - ClusterState.hosp
 ClusterState = recordtype('ClusterState', 'total infect hosp isol danger died heal') 
-# XXX: Total available Medical fascility for Hospitalization = SimConfig.medcnt - ClusterState.hosp
 
 # Clusters Initial State
 JakartaCluster = ClusterState(0, 0, 0, 0, 0, 0, 0)
 
 # Patient data structure records: 
-# - id of patient (City + number) 
-# - day of infection
-# - hospitalization probability
-# - isolation act (yes, no)
-# - traveling probability
-# - infecting probablity
-# - infecting status (1=yes, 2=no)
-# - if probable of dying, die day 
-# - if probable of healing, healing day 
+# id = ID of patient (City + number) 
+# doi = when (day) got infection
+# hop = hospitalization probability
+# iso = isolation act (yes, no)
+# trv = traveling probability
+# inf = infecting probablity
+# infstat = infecting status (1=yes, 2=no)
+# dday = if probable of dying, die day 
+# hday = if probable of healing, healing day 
 Patient = recordtype('Patient', 'id doi hop iso trv inf infstat dday hday')
-# Array of Patients
+
+# Dict of Patients
 PatientArr = {} 
 
+# Dict of Patients die or healed
+HealOrDieList = {}
+
+# function to generate probability Array
 def randomSeedArray(pctFail):
     someArray = []
     choices = [1,2] #1 === good option  2 === bad bad bad
     cnt = 0
-
     for i in range(1,100):
         c = random.choice(choices)
         if c == 2: 
@@ -63,11 +85,12 @@ def Jakarta(env, start_with):
     global Config
     global JakartaCluster
     global PatientArr
-  
+    global HealOrDieList
+
     HealOrDieArray = randomSeedArray(8)
 
     fname = inspect.stack()[0][3] # get function name
-    SimConfig = Config[fname]
+    Config = Config[fname]
 
     print('Jakarta started with %d patient at %d' % (start_with, env.now))
 
@@ -84,7 +107,7 @@ def Jakarta(env, start_with):
 
     curIndex = 1
     while True:
-        add_patient = int(JakartaCluster.infect * SimConfig.ro)
+        add_patient = int(JakartaCluster.infect * Config.ro)
 
         for i in range (0, max(add_patient-1,1)):
             curIndex = int(JakartaCluster.total+1)
@@ -95,25 +118,31 @@ def Jakarta(env, start_with):
             JakartaCluster.danger += 1
             
             
-            Hospitalized(env, JakartaCluster, NewPatient, SimConfig)
+            Hospitalized(env, JakartaCluster, NewPatient, Config)
             if NewPatient.hop != 1 :
-                Isolate(env, JakartaCluster, NewPatient, SimConfig)
+                Isolate(env, JakartaCluster, NewPatient, Config)
 
             if NewPatient.hop == 1 or NewPatient.iso == 1 : NewPatient.inf = 10
             else : NewPatient.inf = 90
 
-            Infect(env, JakartaCluster, NewPatient, SimConfig)
+            Infect(env, JakartaCluster, NewPatient, Config)
             
             probHealOrDie = random.choice(HealOrDieArray)
             random.shuffle(HealOrDieArray)
 
             #print('PROB HEAL DIE %d' % probHealOrDie)
             if probHealOrDie == 1: #heal
+                hday = env.now + randint(Config.hstart, Config.hend)
                 NewPatient.dday = -1
-                NewPatient.hday = env.now + randint(SimConfig.hstart, SimConfig.hend)
+                NewPatient.hday = hday
+                if hday not in HealOrDieList: HealOrDieList[hday] = []
+                HealOrDieList[hday].append(NewPatient.id)
             elif probHealOrDie == 2: #dead
-                NewPatient.dday = env.now + randint(SimConfig.dstart, SimConfig.dend)
+                dday = env.now + randint(Config.dstart, Config.dend)
+                NewPatient.dday = dday
                 NewPatient.hday = -1
+                if dday not in HealOrDieList: HealOrDieList[dday] = []
+                HealOrDieList[dday].append(NewPatient.id)
             PatientArr['Jakarta'+str(curIndex)] = NewPatient
 
         #print(PatientArr)
@@ -150,56 +179,41 @@ def Infect(env, Cluster, Patient, Config):
         Cluster.infect += 1
         Patient.infstat = 1
 
-def Die(env):
+def HealOrDie(env):
     global PatientArr
     global JakartaCluster
+    global HealOrDieList
 
     while True:
-        DelArr = []
-        cnt = 0
-        for x in PatientArr:
-            if PatientArr[x].dday == env.now:
-                #print('Jakarta HEAL 1 patient %s' % x)
-                DelArr.append(x)
-                cnt += 1
-        for x in DelArr:
-            Patient = PatientArr[x]
-            isinfect = Patient.infstat 
-            del PatientArr[x]
-            if isinfect == 1 : JakartaCluster.infect -= 1
-            JakartaCluster.died += 1
+        curday = env.now
+        hcnt = 0
+        dcnt = 0
+        if curday in HealOrDieList:
+            for x in HealOrDieList[curday]:
+                Patient = PatientArr[x]
+                isinfect = Patient.infstat 
+                ishealed = 0
+                if Patient.dday == -1:
+                    ishealed = 1
+                del PatientArr[x]
+                if isinfect == 1 : 
+                    JakartaCluster.infect -= 1
+                if ishealed : 
+                    JakartaCluster.heal += 1
+                    hcnt += 1
+                else :
+                    JakartaCluster.died += 1
+                    dcnt += 1
 
-        print('DEAD: Jakarta Day#%d' % env.now, JakartaCluster)
-        yield env.timeout(1)
-
-
-def Heal(env):
-    global PatientArr
-    global JakartaCluster
-
-    while True:
-        DelArr = []
-        cnt = 0
-        for x in PatientArr:
-            if PatientArr[x].hday == env.now:
-                #print('Jakarta HEAL 1 patient %s' % x)
-                DelArr.append(x)
-                cnt += 1
-        for x in DelArr:
-            Patient = PatientArr[x]
-            isinfect = Patient.infstat 
-            del PatientArr[x]
-            if isinfect == 1 : JakartaCluster.infect -= 1
-            JakartaCluster.heal += 1
-
-        print('HEAL: Jakarta Day#%d' % env.now, JakartaCluster)
+        print('Jakarta Day#%d - HEALED: %d - DIED: %d' % (env.now, hcnt, dcnt), JakartaCluster)
         yield env.timeout(1)
 
 env = simpy.Environment()
 env.process(Jakarta(env, 2))
-env.process(Heal(env))
-env.process(Die(env))
-env.run(until=48)
+env.process(HealOrDie(env))
+#env.process(Heal(env))
+#env.process(Die(env))
+env.run(until=365)
 
 print('Jakarta Day#%d' % env.now, JakartaCluster)
 #print(PatientArr)
